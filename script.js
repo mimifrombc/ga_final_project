4994bff6fd15759525cfcea41a0e9d5df300db2b
Mika <mika@Mikas-MacBook-Pro.local>
2020-09-02 16:38:48 -0700

    upd

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function toggleClass(){
   let menu = document.querySelector(".mainMenu");
   menu.classList.toggle("toggleCls");
+
+  console.log('test')
 }
+
+// const hamburger = document.getElementById('hamburger');
+// const navUl = document.getElementById('nav-ul');
+
+// hamburger.addEventListener('click', ()=> {
+//   navUl.classList.toggle('show');
+// });
